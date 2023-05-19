752c6fb6504ff048a42e7b0ca71426c70287db20
Mauricio Furlan <mauricio.furlan@MIDGARD.LOCAL>
2023-05-19 00:46:07 -0300

    adicionando primeiro teste

--- botlib/botlib.py
+++ botlib/botlib.py
@@ -1,19 +1,19 @@
 import pyautogui
 
 
-def move_and_click(x, y, duration=0.0, clicks=1, button='left', *args):
+def move_and_click(x, y, duration=0.0, clicks=1, button='left'):
     """
     Move o mouse até a posição desejada na tela e realiza um ou mais cliques.
     Parameters:
-        x (int): a posição horizontal (coordenada x) na tela para mover o cursor do mouse.
-        y (int): a posição vertical (coordenada y) na tela para mover o cursor do mouse.
-        duration (float): o tempo em segundos que levará o cursor do mouse para chegar à posição desejada.
-        clicks (int): o número de cliques a serem realizados.
-        button (str): o botão do mouse a ser pressionado. Os valores possíveis são:
-          'left' para o botão esquerdo, 'right' para o botão direito e 'middle' para o botão central do mouse.
+        x (int, obrigatório): a posição horizontal (coordenada x) na tela para mover o cursor do mouse.
+        y (int, obrigatório): a posição vertical (coordenada y) na tela para mover o cursor do mouse.
+        duration (float, opcional): o tempo em segundos que levará o cursor do mouse para chegar à posição desejada. Valor padrão é 0.0
+        clicks (int, opcional): o número de cliques a serem realizados. Valor padrão é 1.
+        button (str, opcional): o botão do mouse a ser pressionado. Os valores possíveis são:
+          'left' para o botão esquerdo, 'right' para o botão direito e 'middle' para o botão central do mouse. Valor padrão é 'left'.
 
-    >>> move_and_click(x=350, y=300, duration=0)
+    >>> move_and_click(300, 300)
     """
-    pyautogui.moveTo(x, y, duration, *args)
+    pyautogui.moveTo(x, y, duration)
     pyautogui.click(x=None, y=None, clicks=clicks, button=button)
     return
